23f07bd33e537682ab685f26b7fd9e5dd583984e
wentao.zhang <taowenzhang@gmail.com>
2012-12-09 16:55:46 +0800

    add private setting file

---
+++ key.py
@@ -0,0 +1,18 @@
+# some private account settings
+setting= {
+			'local':{
+				'database':{
+					'name':'',
+					'user':'',
+					'passwd':''
+				}
+			},
+
+			'remote':{
+				'database':{
+					'name':'',
+					'user':'',
+					'passwd':''
+				}
+			}
+		}
