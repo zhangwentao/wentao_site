bb4edff3ff25279e5ca948cbdec5a8acd39fbfbc
zhangwentao <taowenzhang@gmail.com>
2013-04-11 18:22:23 +0800

    增加 accountbook app

--- urls.py
+++ urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 	url('^blog/',include('blog.urls')),
 	url('^',include('blog.urls')),
+	url('^accountbook/',include('accountbook.urls')),
 )
 
 handler404 = 'blog.views.handle404'
